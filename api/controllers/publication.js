f0ed54396fd6c630dd038c8ce5f26512c4016511
Davidama <davidarteromartinez@gmail.com>
2021-04-26 20:38:15 +0200

    publications methods

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -15,6 +15,79 @@ function probando(req, res){
     });
 }
 
+function savePublication(req, res){
+    var params = req.body;
+   
+    //Settear datos
+
+    if(!params.text) return res.status(200).send({message: 'Debes enviar un texto'});
+
+    var publication = new Publication();
+    publication.text = params.text;
+    publication.file = 'null';
+    publication.user = req.user.sub;
+    publication.created_at = moment().unix();
+
+    publication.save((err, publicationStored) =>{
+        if(err) return res.status(500).send({message: 'Error al guardar la publicación'});
+
+        if(!publicationStored) return res.status(404).send({message: 'La publicación no ha sido guardada'}); 
+
+        return res.status(200).send({publication: publicationStored});
+    });
+}
+
+function getPublications(req, res){
+    var page = 1;
+    //Si recibimos la página por los parámetros
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 4;
+
+    //find de usuarios que seguimos
+    Follow.find({user: req.user.sub}).populate('followed').exec((err, follows) => {
+        if(err) return res.status(500).send({message: 'Error al devolver el seguimiento'});
+        //Array de ids que seguimos
+        var follows_clean = [];
+        follows.forEach((follow) => {
+            follows_clean.push(follow.followed);
+        });
+        //console.log("array gente que seguimos->",follows_clean);
+        Publication.find({user: {"$in": follows_clean}}).sort('-created_at').populate('user').paginate(page, itemsPerPage, (err, publications, total) =>{
+            if(err) return res.status(500).send({message: 'Error al devolver publicaciones'});
+
+            if(!publications) return res.status(404).send({message: 'No hay publicaciones'});
+
+            return res.status(200).send({
+                total_items: total,
+                pages: Math.ceil(total/itemsPerPage),
+                page: page,
+                publications
+            })
+
+        });
+
+    });
+
+}
+
+function getPublication(req, res){
+    var publicationId = req.params.id;
+
+    Publication.findById(publicationId, (err, publication) => {
+        if(err) return res.status(500).send({message: 'Error al devolver la publicación'});
+
+        if(!publication) return res.status(404).send({message: 'No existe la publicación'});
+
+        return res.status(200).send({publication});
+    });
+}
+
 module.exports = {
-    probando
+    probando,
+    savePublication,
+    getPublications,
+    getPublication
 }
