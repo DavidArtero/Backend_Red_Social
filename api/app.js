68ecc47917828c52d49fb6dd851abbe5d335890e
Davidama <davidarteromartinez@gmail.com>
2021-04-27 17:43:04 +0200

    messagesController

--- api/app.js
+++ api/app.js
@@ -11,6 +11,7 @@ var app = express();
 var user_routes = require('./routes/user');
 var follow_routes = require('./routes/follow');
 var publication_routes = require('./routes/publication');
+var message_routes = require('./routes/message');
 
 // Middlewares (antes del controlador)
 app.use(bodyParser.urlencoded({extended:false}));
@@ -22,6 +23,7 @@ app.use(bodyParser.json());
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
 app.use('/api', publication_routes);
+app.use('/api', message_routes)
 
 // Exportar
 module.exports = app;
---
+++ api/controllers/message.js
@@ -0,0 +1,42 @@
+'use strict'
+
+var moment = require('moment');
+var mongoosePaginate = require('mongoose-pagination');
+
+var User = require('../models/user');
+var Follow = require('../models/follow');
+var Message = require('../models/message');
+
+function probando(req, res){
+    return res.status(200).send({message:'hola desde el controlador message'})
+};
+
+function saveMessage(req, res){
+    var params = req.body;
+
+    if(!params.text || !params.receiver){
+        return res.status(200).send({message:'Envia los datos necesarios'});
+    }
+
+    var message = new Message();
+    message.emitter = req.user.sub;
+    message.receiver = params.receiver;
+    message.text = params.text;
+    message.created_at = moment().unix();
+
+    message.save((err, messageStored) => {
+        if(err) return res.status(500).send({message:'Error en la petición'});
+
+        if(!messageStored) return res.status(500).send({message:'Error al enviar el mensaje'});
+
+        res.status(200).send({message: messageStored});
+
+    });
+
+
+}
+
+module.exports = {
+    probando,
+    saveMessage
+}
