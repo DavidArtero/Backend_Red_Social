07b68af86e87376adbe3e0f4049bbb26a7c13d4d
Davidama <davidarteromartinez@gmail.com>
2021-04-27 21:16:29 +0200

    messageController

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -23,6 +23,7 @@ function saveMessage(req, res){
     message.receiver = params.receiver;
     message.text = params.text;
     message.created_at = moment().unix();
+    message.viewed = 'false';
 
     message.save((err, messageStored) => {
         if(err) return res.status(500).send({message:'Error en la petición'});
@@ -32,11 +33,86 @@ function saveMessage(req, res){
         res.status(200).send({message: messageStored});
 
     });
+}
 
+function getReceivedMessages(req, res){
+    var userId= req.user.sub;
+    var page= 1;
 
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 4;
+
+    Message.find({ receiver: userId }).populate('emitter', 'name surname nick image _id').paginate(page, itemsPerPage, (err, messages, total) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+        if(!messages) return res.status(404).send({message: 'no hay mensajes'});
+
+        
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            messages
+        })
+    });
+}
+
+function getEmmitMessages(req, res){
+    var userId= req.user.sub;
+    var page= 1;
+
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 4;
+
+    Message.find({ emitter: userId }).populate('emitter receiver', 'name surname nick image _id').paginate(page, itemsPerPage, (err, messages, total) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+        if(!messages) return res.status(404).send({message: 'no hay mensajes'});
+
+        
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            messages
+        })
+    });
+}
+
+function getUnviewedMessages(req, res){
+    var userId = req.user.sub;
+
+    Message.countDocuments({receiver: userId, viewed: 'false'}).exec((err, count) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+        
+        return res.status(200).send({
+            'unviewed': count
+        });
+    })
+}
+
+function setViewedMessages(req, res){
+    var userId = req.user.sub;
+
+    Message.updateMany({receiver:userId, viewed: 'false'}, {viewed:'true'},{"multi":true}, (err, messagesUpdated) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+
+        return res.status(200).send({
+            messages: messagesUpdated
+        });
+
+    })
 }
 
 module.exports = {
     probando,
-    saveMessage
+    saveMessage,
+    getReceivedMessages,
+    getEmmitMessages,
+    getUnviewedMessages,
+    setViewedMessages
 }
--- api/models/message.js
+++ api/models/message.js
@@ -8,8 +8,8 @@ var MessageSchema = Schema({
     text: String,
     viewed: String,
     created_at: String,
-    emitter: { type: Schema.ObjectId, ref:'User' },
-    receiver: { type: Schema.ObjectId, ref:'User' }
+    emitter: { type: Schema.ObjectId, ref: 'User' },
+    receiver: { type: Schema.ObjectId, ref: 'User' }
 });
 
 
